93a9c21f0e58ba980b17031ce492ded1e0eebc40
rimi <rimi@laughing.local>
2015-07-21 11:23:55 +0800

    Initial Commit

---
+++ CAShapeLayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CAShapeLayer
+//
+//  Created by rimi on 15/7/21.
+//  Copyright (c) 2015年 LeeSefung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CAShapeLayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CAShapeLayer
+//
+//  Created by rimi on 15/7/21.
+//  Copyright (c) 2015年 LeeSefung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
